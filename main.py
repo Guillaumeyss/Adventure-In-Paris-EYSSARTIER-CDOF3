cd5d9c6b10acdb076a093b0de4dfbe53a847fc93
Guillaumeyss <114230433+Guillaumeyss@users.noreply.github.com>
2024-01-22 18:32:42 +0100

    finished intro

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 
 
+inventory = []
+
+def displayPerson() :
+    print("  O")
+    print(" /|\\")
+    print(" / \\")
+
+
+def display_inventory():
+    global inventory  
+    print("Your current inventory: ", inventory)
 
 def intro():
+    global inventory
     print("Hello "+name)
+    print("Here is your first choice, do you want to have a knife or a gun ?")
+    print ("1 - knife")
+    print("2 - gun")
+    choice = input("Enter your choice (1 or 2): ")
+    if choice == "1":
+        inventory.append("knife")
+        print("You chose a knife. It has been added to your inventory.")
+    elif choice == "2":
+        inventory.append("gun")
+        print("You chose a gun. It has been added to your inventory.")
+    else:
+        print("Invalid choice. Please enter 1 or 2.")
+    display_inventory()
+    print("I would have chosen the other one but ... it's your choice ! Maybe you are right, maybe...")
+    first_scene()
+    
+def first_scene():
+    displayPerson()
+
 
 
 
