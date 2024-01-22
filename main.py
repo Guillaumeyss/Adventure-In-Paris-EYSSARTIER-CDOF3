906b57c3d97568f64ca6455aac180c86ab179fdb
Guillaumeyss <114230433+Guillaumeyss@users.noreply.github.com>
2024-01-22 17:39:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+if __name__ == "__main__":
+  while True:
+    print("Welcome to my Text based Adventure Game")
+    print("You will travel in the shadows of Paris !")
+    print("During your travel, you will have multiple decisions to take in order to stay alive.")
+    print("I hope you will take the good ones...")
+    print("But first, let me know what is your name: ")
+    name = input()
+    print("Good luck, " +name+ ".")
